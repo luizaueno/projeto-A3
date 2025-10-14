eb9ce18aef437e04ada242cf191f4163119d4581
luiza <161471348+luizaueno@users.noreply.github.com>
2025-10-14 12:22:41 -0300

    inicializando repositório

---
+++ src/test/java/com/example/projetoA3/ProjetoA3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoA3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoA3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
