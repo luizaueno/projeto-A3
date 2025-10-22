f51a23d18c24ef25b66e1d046edd709edee9f9e1
luiza <161471348+luizaueno@users.noreply.github.com>
2025-10-22 15:03:18 -0300

    tela de login css

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import Login from './Login'
+import './login.css'
+import Cadastro from './Cadastro'
+import './cadastro.css'
+
+
+function App() {
+  
+  return (
+    <>
+    <div>
+      <h1>Bem-vindo</h1>
+      <Login />
+    </div>
+    <div>
+      <h1>Cadastre-se</h1>
+      <Cadastro />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Login.tsx
@@ -0,0 +1,14 @@
+
+export default function Login() {
+  return(
+    <>
+    <div className="container">
+      <input className="input" type="email" placeholder="email" />
+      <input className="input" type="password" placeholder="senha" />
+      <button id="botao">Entrar</button>
+      <a href="Cadastro.tsx" >Cadastre-se aqui</a>
+    </div>
+    </>
+   
+  )
+} 
